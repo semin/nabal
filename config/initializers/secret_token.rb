cce1a0c5cf7cec90386d0e988629bf8cc69a96b5
Semin Lee <seminlee@gmail.com>
2010-10-04 11:49:54 +0100

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Nabal::Application.config.secret_token = 'af0e6e5a842fea2d9ed1fa07c8f75113bad3d3e8ac7d73a64ee8a83c233f140a3f43103d83b774b35e32358243a5f39e203e1da946e2ad65f15096e27b37fbb9'
